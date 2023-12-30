f37a6da43f86091fb41b246eaa4c0f065272d8da
Douglas Wellington Martins <90710466+dwmartins@users.noreply.github.com>
2023-12-30 14:29:28 -0300

    feat: Finalizado o desenvolvimento do footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'guia_agudos_front';
