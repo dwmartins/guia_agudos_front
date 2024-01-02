daa500e1de056f9b2f5c4f71ad9926910909fa10
Douglas Wellington Martins <90710466+dwmartins@users.noreply.github.com>
2024-01-02 20:07:37 -0300

    feat: Desenvolvimento da view do carousel

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ListingCategoryComponent } from '../../components/listing-category/listing-category.component';
+import { CarouselComponent } from '../../components/carousel/carousel.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, ListingCategoryComponent],
+  imports: [CommonModule, ListingCategoryComponent, CarouselComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
