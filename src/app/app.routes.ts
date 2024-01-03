e22b46ba502c2a8e041f2c5eaad40d73909c2f4b
Douglas Wellington Martins <90710466+dwmartins@users.noreply.github.com>
2024-01-03 18:51:36 -0300

    feat: Configurando rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { LoginComponent } from './views/login/login.component';
 import { ForgotPasswordComponent } from './views/forgot-password/forgot-password.component';
 import { RegisterComponent } from './views/register/register.component';
 import { ListingCategoryAllComponent } from './views/listing-category-all/listing-category-all.component';
+import { PlansComponent } from './views/plans/plans.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'login', component: LoginComponent},
     {path: 'nova-conta', component: RegisterComponent},
     {path: 'recuperacao-senha', component: ForgotPasswordComponent},
-    {path: 'anuncios/categorias', component: ListingCategoryAllComponent}
+    {path: 'anuncios/categorias', component: ListingCategoryAllComponent},
+    {path: 'planos', component: PlansComponent}
 ];
