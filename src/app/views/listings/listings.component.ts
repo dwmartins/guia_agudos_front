c8cebbacbafcc893b8259fe193cb76b5a04a59b2
Douglas Wellington Martins <90710466+dwmartins@users.noreply.github.com>
2024-01-09 20:42:46 -0300

    feat: Iniciando o desenvolvimento da view de listing

--- src/app/views/listings/listings.component.ts
+++ src/app/views/listings/listings.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ListingCategoryService } from '../../services/listing-category.service';
+import { ListingCategory } from '../../../models/listingCategory';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-listings',
-  standalone: true,
-  imports: [],
-  templateUrl: './listings.component.html',
-  styleUrl: './listings.component.css'
+   selector: 'app-listings',
+   standalone: true,
+   imports: [CommonModule, RouterModule],
+   templateUrl: './listings.component.html',
+   styleUrl: './listings.component.css'
 })
-export class ListingsComponent {
+export class ListingsComponent implements OnInit{
+   categoryService = inject(ListingCategoryService);
 
+   iconCategories: boolean = false;
+
+   categories: ListingCategory[] = [];
+
+   testeQtd: number = 10;
+
+   ngOnInit(): void {
+      this.getCategories();
+   }
+
+   toggleIconCategories() {
+      this.iconCategories = !this.iconCategories;
+   }
+
+   getCategories() {
+      this.categoryService.categories(null).subscribe((response) => {
+         this.categories = response;
+         console.log(this.categories);
+      }, (error) => {
+         console.error('ERROR: ', error);
+      })
+   }
 }
