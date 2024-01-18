757114ebeafb59ad4ebd021a6a496c9e91904e06
Douglas Martins <douglas5422@outlook.com>
2024-01-17 23:15:12 -0300

    feat: Implementando o Lightbox para visualizar as imagens

--- src/app/views/listing-info/listing-info.component.ts
+++ src/app/views/listing-info/listing-info.component.ts
@@ -1,20 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { Lightbox, LightboxModule } from 'ngx-lightbox';
 
 @Component({
   	selector: 'app-listing-info',
   	standalone: true,
-  	imports: [RouterModule, CommonModule],
+  	imports: [RouterModule, CommonModule, LightboxModule],
   	templateUrl: './listing-info.component.html',
   	styleUrl: './listing-info.component.css'
 })
 export class ListingInfoComponent implements OnInit{	
 	route = inject(ActivatedRoute);
 	router = inject(Router);
+	lightbox = inject(Lightbox);
 
 	listingId: number = 0;
 	iconCategories: boolean = false;
+
+	galleryImages = [
+		'https://images.unsplash.com/photo-1682687220247-9f786e34d472?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8',
+		'https://images.unsplash.com/photo-1682687982134-2ac563b2228b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+		'https://images.unsplash.com/photo-1682686580036-b5e25932ce9a?q=80&w=1375&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+		'https://images.unsplash.com/photo-1682687220199-d0124f48f95b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHx8',
+		'https://images.unsplash.com/photo-1682685797828-d3b2561deef4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxMXx8fGVufDB8fHx8fA%3D%3D'
+	]
 
 	ngOnInit(): void {
 		this.getParams(); 
@@ -30,4 +40,12 @@ export class ListingInfoComponent implements OnInit{
 	toggleIconOpeningHours() {
       this.iconCategories = !this.iconCategories;
    }
+
+	openLightbox(index: number): void {
+		const album = this.galleryImages.map(link => {
+		  return { src: link, caption: '', thumb: '' };
+		});
+  
+		this.lightbox.open(album, index);
+	 }
 }
