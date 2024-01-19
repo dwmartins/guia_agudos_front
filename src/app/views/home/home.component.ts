b23716da8ca4daf560997b87c9b2536fadd09459
Douglas Wellington Martins <90710466+dwmartins@users.noreply.github.com>
2024-01-19 17:52:18 -0300

    feat: Criando banner da home

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ListingCategoryComponent } from '../../components/listing-category/listing-category.component';
 import { CarouselComponent } from '../../components/carousel/carousel.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, ListingCategoryComponent, CarouselComponent],
+  imports: [CommonModule, ListingCategoryComponent, CarouselComponent, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
