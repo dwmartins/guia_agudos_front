0a1a5b66e40bb7419f148d3bdb5cb1fdc5da4a17
Douglas Martins <douglas5422@outlook.com>
2024-01-18 22:37:30 -0300

    feat: Finalizando o modal para adicionar comentario na listing

--- src/app/views/listing-info/listing-info.component.ts
+++ src/app/views/listing-info/listing-info.component.ts
@@ -1,26 +1,60 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component, ElementRef, OnInit, ViewChild, inject } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Lightbox, LightboxModule } from 'ngx-lightbox';
+import { AlertsComponent } from '../../components/alerts/alerts.component';
+import { Assessment } from '../../../models/Assessment';
+import { AssessmentService } from '../../services/assessment.service';
+import { User } from '../../../models/user';
 
 @Component({
   	selector: 'app-listing-info',
   	standalone: true,
-  	imports: [RouterModule, CommonModule, LightboxModule],
+  	imports: [RouterModule, CommonModule, LightboxModule, ReactiveFormsModule, HttpClientModule, AlertsComponent],
   	templateUrl: './listing-info.component.html',
   	styleUrl: './listing-info.component.css'
 })
 export class ListingInfoComponent implements OnInit{	
-	route = inject(ActivatedRoute);
-	router = inject(Router);
-	lightbox = inject(Lightbox);
-	modal = inject(NgbModal);
+	@ViewChild('modalAssessment', {static: true}) modalAssessment!: ElementRef;
+	@ViewChild('commentAssessment', {static: true}) commentAssessment!: ElementRef;
 
-	@ViewChild('modalAssessment', {static: true}) modalAssessment!: ElementRef
+	route 				= inject(ActivatedRoute);
+	router 				= inject(Router);
+	lightbox 			= inject(Lightbox);
+	modal 				= inject(NgbModal);
+	formBuilder 		= inject(FormBuilder);
+	assessmentService 	= inject(AssessmentService)
 
 	listingId: number = 0;
 	iconCategories: boolean = false;
+	user: Partial<User> = {};
+
+	alert: any[] = [];
+
+	selectedStars: number = 0;
+  	hoverStars: number = 0;
+  	totalStars: number = 5;
+  	currentIcon: string = 'bi-star';
+  	hoverIcon: string = 'bi-star-fill';
+	notAssessment!: boolean;
+
+	formAssessment: FormGroup;
+	assessment: Assessment[] = [];
+
+	maxLengthComment: number = 130;
+	currentLengthComment: number = 130
+
+	constructor() {
+		this.formAssessment = this.formBuilder.group({
+			listing_id: [0],
+			user_id: [''],
+			comment: ['', [Validators.required]],
+			assessment: [0, [Validators.required]]
+		})
+	}
 
 	galleryImages = [
 		'https://images.unsplash.com/photo-1682687220247-9f786e34d472?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8',
@@ -31,12 +65,11 @@ export class ListingInfoComponent implements OnInit{
 	]
 
 	ngOnInit(): void {
-		this.getParams(); 
+		this.getParams();
 	}
 
 	getParams() {
 		this.route.params.subscribe(params => {
-			console.log(params['id']);
 			this.listingId = params['id'];
 		})
 	}
@@ -54,6 +87,86 @@ export class ListingInfoComponent implements OnInit{
 	}
 	
 	OpenModalUserAssessment() {
-		this.modal.open(this.modalAssessment, {centered: true});
+		if(this.getUserLogged()) {
+			this.cleanFormAssessment();
+			this.modal.open(this.modalAssessment, {centered: true});
+		} else {
+			this.router.navigate(['/login'])
+		}
+	}
+
+	submitAssessment() {
+		this.formAssessment.get('listing_id')?.setValue(this.listingId);
+		this.formAssessment.get('user_id')?.setValue(this.user.id);
+
+		if(!this.selectedStars) {
+			this.notAssessment = true;
+			return;
+		}
+
+		if(this.formAssessment.valid) {
+			this.assessmentService.newAssessment(this.formAssessment.value).subscribe((response) => {
+				this.modal.dismissAll(this.modalAssessment);
+				this.alerts('success', 'Avaliação inserida com sucesso.');
+			},(error) => {
+				this.alerts('error', 'Falha ao inserir sua avaliação.');
+				console.error('ERROR: ', error);
+			})
+		}
+	}
+
+	cleanFormAssessment() {
+		this.formAssessment.reset({
+		  comment: '',
+		  assessment: ''
+		});
+
+		this.selectedStars = 0;
+	}
+
+	selectStars(stars: number): void {
+		this.notAssessment = false;
+		this.selectedStars = stars;
+		this.formAssessment.get('assessment')?.setValue(this.selectedStars);
+	}
+	
+	hoverStar(stars: number): void {
+		this.hoverStars = stars;
+	}
+	
+	resetHover(): void {
+		this.hoverStars = 0;
+	}
+
+	getUserLogged() {
+		const user = localStorage.getItem('userData');
+
+		if(user) {
+			this.user = JSON.parse(user) as User;
+			return true;
+		}
+
+		return true;
+	}
+
+	maxLengthValidator(maxLength: number) {
+		return (control: AbstractControl): ValidationErrors | null => {
+			const value: string = control.value || '';
+			const length = value.trim().length;
+		
+			return length > maxLength ? { maxLengthExceeded: true } : null;
+		};
+	}
+
+	countCaracterComment() {
+		const currentLength = this.formAssessment.get('comment')?.value.length
+		this.currentLengthComment = Math.max(0, this.maxLengthComment - currentLength);
+	}
+
+	alerts(type: string, description: string) {
+		this.alert.push({
+		   type: type,
+		   description: description
+		})
 	}
 }
