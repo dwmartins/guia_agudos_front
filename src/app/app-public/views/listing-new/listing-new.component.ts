f32d4e4a8475c2e464fcdda71f990382e3fe48a7
Douglas Wellington Martins <90710466+dwmartins@users.noreply.github.com>
2024-02-04 00:56:42 -0300

    validando as categorias do anuncio

--- src/app/app-public/views/listing-new/listing-new.component.ts
+++ src/app/app-public/views/listing-new/listing-new.component.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { ListingCategoryService } from '../../../services/listing-category.service';
 import { ListingCategory } from '../../../models/listingCategory';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { AlertService } from '../../../services/componsents/alert.service';
 
 @Component({
     selector: 'app-listing-new',
@@ -14,10 +15,13 @@ import { FooterComponent } from '../../components/footer/footer.component';
 })
 export class ListingNewComponent implements OnInit{
     categoryService = inject(ListingCategoryService);
+    alertService    = inject(AlertService);
 
     categories: ListingCategory[] = [];
     searchItensCategory: ListingCategory[] = [];
     searchItem: string = '';
+
+    categoriesSelect: ListingCategory[] = [];
 
     ngOnInit(): void {
         this.goToTheTopWindow();
@@ -37,6 +41,26 @@ export class ListingNewComponent implements OnInit{
         this.searchItensCategory = this.categories.filter(object =>
            object.cat_name.toLowerCase().includes(this.searchItem.toLowerCase())
         );
+    }
+
+    setCategory(category: ListingCategory) {
+        const max = 3;
+
+        if(this.categoriesSelect.length === max) {
+            this.alertService.showAlert('info', 'Você atingiu o limite máximo de categorias do seu plano.');
+            return;
+        }
+
+        const exists = this.categoriesSelect.some(existsCategory => existsCategory.id === category.id);
+        if(!exists) {
+            this.categoriesSelect.push(category);
+            return;
+        }
+        this.alertService.showAlert('info', 'Você já selecionou essa categoria.');
+    }
+
+    removeCategory(id: number) {
+        this.categoriesSelect = this.categoriesSelect.filter(category => category.id !== id);
     }
 
     goToTheTopWindow() {
