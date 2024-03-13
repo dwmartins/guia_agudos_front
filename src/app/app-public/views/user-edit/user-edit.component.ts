071a9457e51b4bd4702d2cdcd5b2733f0166f10c
Douglas Wellington Martins <90710466+dwmartins@users.noreply.github.com>
2024-03-12 23:01:27 -0300

    feat: criando tela para editar um usuário

--- src/app/app-public/views/user-edit/user-edit.component.ts
+++ src/app/app-public/views/user-edit/user-edit.component.ts
@@ -1,14 +1,37 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { AuthService } from '../../../services/auth.service';
+import { User } from '../../../models/user';
+import { DateService } from '../../../services/helpers/date.service';
 
 @Component({
-  selector: 'app-user-edit',
-  standalone: true,
-  imports: [CommonModule, FooterComponent],
-  templateUrl: './user-edit.component.html',
-  styleUrl: './user-edit.component.css'
+    selector: 'app-user-edit',
+    standalone: true,
+    imports: [CommonModule, FooterComponent],
+    templateUrl: './user-edit.component.html',
+    styleUrl: './user-edit.component.css'
 })
-export class UserEditComponent {
+export class UserEditComponent implements OnInit {
+    authService = inject(AuthService);
+    dateService = inject(DateService);
 
+    user!: User;
+
+    imgDefaultUser: string = '../../../../assets/img/no-image-user.jpg';
+
+    constructor() {
+
+    }
+
+    ngOnInit(): void {
+        this.getUserLogged()
+    }
+
+    getUserLogged() {
+        const user = this.authService.getUserLogged();
+        if (user) {
+            this.user = user;
+        }
+    }
 }
