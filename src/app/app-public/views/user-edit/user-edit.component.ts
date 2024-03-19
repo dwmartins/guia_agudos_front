88c2677f91171e9c2f96ab54a7444f413162f2fb
Douglas Wellington Martins <90710466+dwmartins@users.noreply.github.com>
2024-03-18 21:44:15 -0300

    Criado metodo para alterar foto na tela de editar

--- src/app/app-public/views/user-edit/user-edit.component.ts
+++ src/app/app-public/views/user-edit/user-edit.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, inject } from '@angular/core';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { AuthService } from '../../../services/auth.service';
 import { User } from '../../../models/user';
@@ -10,6 +10,8 @@ import { AlertService } from '../../../services/components/alert.service';
 import { UserService } from '../../../services/user.service';
 import { ValidErrorsService } from '../../../services/helpers/valid-errors.service';
 import { SpinnerLoadingComponent } from '../../../shared/components/spinner-loading/spinner-loading.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ImageValidationService } from '../../../services/helpers/image-validation.service';
 
 @Component({
     selector: 'app-user-edit',
@@ -19,12 +21,16 @@ import { SpinnerLoadingComponent } from '../../../shared/components/spinner-load
     styleUrl: './user-edit.component.css'
 })
 export class UserEditComponent implements OnInit {
+    modal 			    = inject(NgbModal);
     authService         = inject(AuthService);
     dateService         = inject(DateService);
     alertService        = inject(AlertService);
     userService         = inject(UserService);
     formBuilder         = inject(FormBuilder);
     validErrorsService  = inject(ValidErrorsService);
+    imageService        = inject(ImageValidationService);
+
+    @ViewChild('modalChangePhotoUser', {static: true}) modalChangePhotoUser!: ElementRef;
 
     icon_password: string = 'fa-eye';
     showPassword: string = 'password';
@@ -36,8 +42,12 @@ export class UserEditComponent implements OnInit {
 
     spinnerNewPassword: boolean = false;
     spinnerEdit: boolean = false;
+    spinnerChangePhotoUser: boolean = false;
 
     user!: User;
+
+    previewNewPhotoUser: string | null | undefined;
+    newPhotoUser!: File;
 
     imgDefaultUser: string = '../../../../assets/img/no-image-user.jpg';
 
@@ -139,5 +149,41 @@ export class UserEditComponent implements OnInit {
             this.showNewPassword = (this.showNewPassword === "text") ? "password" : (this.showNewPassword === "password") ? "text" : this.showNewPassword;
             this.icon_NewPassword = (this.icon_NewPassword === "fa-eye") ? "fa-eye-slash" : (this.icon_NewPassword === "fa-eye-slash") ? "fa-eye" : this.icon_NewPassword; 
         }
+    }
+
+    previewPhotoUser(event: Event) {
+        const fileInput = event.target as HTMLInputElement;
+        const file = fileInput.files?.[0];
+
+        if(file) {
+            this.newPhotoUser = file;
+            if(this.imageService.validImage(file)){
+                const reader = new FileReader();
+
+                reader.onload = () => {
+                    this.previewNewPhotoUser = reader.result?.toString();
+                };
+                reader.readAsDataURL(file);
+            }
+
+            this.modal.open(this.modalChangePhotoUser, {centered: true});
+        }
+	}
+
+    changePhotoUser() {
+        this.spinnerChangePhotoUser = true;
+        this.userService.updatePhoto(this.newPhotoUser).subscribe((response) => {
+            this.modal.dismissAll(this.modalChangePhotoUser);
+            this.spinnerChangePhotoUser = false;
+            this.alertService.showAlert('success', 'Foto de perfil atualizada com sucesso.');
+
+            
+            this.authService.updateUserLogged(response);
+            this.getUserLogged();
+        }, (error) => {
+            this.modal.dismissAll(this.modalChangePhotoUser);
+            this.spinnerChangePhotoUser = false;
+            this.validErrorsService.validError(error, "Falha ao atualizar sua foto de perfil.");
+        })
     }
 }
